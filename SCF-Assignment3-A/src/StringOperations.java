6cb41c73f4e351572b5b255e67848cf7047c09c6
Neha Patidar <neha.patidar@metacube.com>
2021-03-03 15:48:23 +0530

    correction

--- SCF-Assignment3-A/src/StringOperations.java
+++ SCF-Assignment3-A/src/StringOperations.java
@@ -1,79 +1,53 @@
-import java.util.Scanner;
 
-public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import java.util.*;
+public class StringOperations {
+	
+	int compareString(String string1,String string2){
+		if (string1.length() != string2.length()) return 0;
+		for(int i = 0; i < string1.length(); i++){
+			if(string1.charAt(i) != string2.charAt(i)) return 0;
+		}
+		return 1;
+		
+	}
+	
+	String reverseString(String string){
 		try{
-			Scanner scanner=new Scanner(System.in);
-			System.out.println("Enter 0 to exit : ");
-			System.out.println("Enter 1 to compare two strings : ");
-			System.out.println("Enter 2 to reverse string : ");
-			System.out.println("Enter 3 to swap cases of string : ");
-			System.out.println("Enter 4 to get largest word from string : ");
-			
-			//input for selecting which operation to be perform
-			int operationCase = scanner.nextInt();
-			
-			//object creation for StringOperation class
-			StringOperations object=new StringOperations();
-			while (operationCase !=0){
-				if(operationCase ==1){
-					// compare strings
-					try{
-						System.out.println("Enter two strings : ");
-						String string1=scanner.next();
-						String string2 = scanner.next();
-						System.out.println("Result of comparision : " + object.compareString(string1, string2));
-					}catch (Exception e){
-					}
-				}
-				else if(operationCase == 2){
-					// reverse a string
-					try{
-						System.out.println("Enter a string : ");
-						String string=scanner.next();
-						System.out.println("Result : " + object.reverseString(string));
-					}catch (Exception e){
-						System.out.println("Invalid Input");
-					}
-				}
-				else if(operationCase == 3){
-					// swap case of letters of string
-					try{
-						System.out.println("Enter a string : ");
-						StringBuilder string=new StringBuilder(scanner.next());
-						System.out.println("Result : " + object.swapCases(string));
-					}
-					catch (Exception e){
-						System.out.println("Invalid Input");
-					}
-				}
-				else if(operationCase == 4){
-					// largest word from string
-					try{
-						System.out.println("Enter a string having word separated by @ : ");
-//					StringBuffer inputString = new StringBuffer(scanner.next());
-						String inputString=scanner.next();
-						String [] string = inputString.toString().split("@");
-						System.out.println("Result : " + object.largestString(string));
-					}catch (Exception e){
-						System.out.println("Invalid input");
-					}
-				}
-				else{
-					System.out.println("Enter a valid input ");
-				}
-				
-				System.out.println("Enter a num from 0-4 to perform any operation : ");
-				operationCase = scanner.nextInt();
+			String result = "";
+			for(int i = string.length() - 1; i >= 0; i--){
+				result += string.charAt(i);
 			}
-			
+			return result;
+		}catch(Exception e){
+			return "Invalid";
 		}
-		catch (Exception e){
-			System.out.println(e);
+	}
+	
+	String swapCases(StringBuilder string){
+		for(int i = 0; i < string.length(); i++){
+			try{
+				if(string.charAt(i) >= 65 && string.charAt(i) <= 90){
+					char temp = (char) (string.charAt(i) + 32);					//swapping the case of letter
+					string.replace(i, i+1,  temp  + "");					// replacing the letter with swapped case letter
+				}else if (string.charAt(i) >= 97 && string.charAt(i) <= 122){
+					char temp = (char) (string.charAt(i) - 32);					//swapping the case of letter
+					string.replace(i, i+1, temp +"");							//replacing the letter with swapped case letter
+				}
+			}catch (Exception e){
+				return "Invalid";
+			}
 		}
-
+		return string.toString();
+	}
+	
+	String largestString(String [] string){
+		String result = "";
+		for(int i = 0; i < string.length; i++){
+			if(result.length() <= string[i].length()){		//check if ith word is greater than previous large word
+				result = string[i];							// assign larger word to result
+			}
+		}
+		return result;
 	}
 
 }
