98fc5c83325278176384ee477ce42de0ea3d48e9
meta-neha-patidar <79203295+meta-neha-patidar@users.noreply.github.com>
2021-03-12 16:15:57 +0530

    assignment 7

---
+++ SCF-Assignment7/src/JUnitTest.java
@@ -0,0 +1,112 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+/*
+ * @author -  Neha Patidar
+ */
+
+public class JUnitTest {
+	SparseMatrix object;
+	
+	@Test
+	public void getTransposeTest() {
+		int array[][] = {
+                {1, 2, 3, 5, 4},
+                {0, 0, 5, 7, 0},
+                {0, 1, 0, 0, 0},
+                {0, 2, 6, 0, 0},
+                {0, 8, 0, 9, 0}
+            };
+		int nonZeroCount = 12;
+		int result[][] ={
+				{0, 0, 1 }, 
+				{1, 0, 2 },
+				{2, 0, 3 }, 
+				{3, 0, 5 },
+				{4, 0, 4 },
+				{2, 1, 5 },
+				{3, 1, 7 },
+				{1, 2, 1 },
+				{1, 3, 2 },
+				{2, 3, 6 },
+				{1, 4, 8 },
+				{3, 4, 9 }
+				};
+		object = new SparseMatrix(nonZeroCount,array);
+		int[][] transpose = object.getTranspose();
+		assertArrayEquals(result, transpose);
+	}
+	
+	@Test
+	public void isSymmetricalMatrixTest(){
+		int array[][] = {
+                {1, 0, 5, 0, 4},
+                {0, 4, 0, 3, 8},
+                {5, 0, 0, 0, 0},
+                {0, 3, 0, 0, 2},
+                {4, 8, 0, 2, 0}
+            };
+		int nonZeroCount = 12;
+		object = new SparseMatrix(nonZeroCount,array);
+		assertEquals(true,object.isSymmetricalMatrix());
+		
+	}
+	@Test
+	public void addMatricesTest() {
+		int array1[][] = {
+				{1, 0, 5, 0, 4},
+                {0, 4, 0, 3, 0},
+                {0, 0, 0, 0, 0},
+                {0, 2, 0, 0, 2},
+                {0, 8, 0, 9, 0}
+            };
+		int array2[][] = {
+				{2, 3, 5, 0, 4},
+                {0, 4, 0, 3, 0},
+                {0, 0, 4, 0, 0},
+                {0, 2, 0, 0, 2},
+                {0, 8, 0, 9, 6}
+            };
+		int result[][] = {
+				{3, 3, 10, 0, 8},
+				{0, 8, 0, 6, 0 },
+				{0, 0, 4, 0, 0 },
+				{0, 4, 0, 0, 4 },
+				{0, 16, 0, 18, 6 }
+		};
+		
+		int addedArray[][] = SparseMatrix.addMatrices(array1, array2);
+		for(int i = 0 ; i < addedArray.length;i++);
+		assertArrayEquals(result,addedArray);
+	}
+	
+	@Test
+	public void multiplyMatricesTest(){
+		int array1[][] = {
+				{1, 0, 5, 0, 4},
+                {0, 4, 0, 3, 0},
+                {0, 0, 0, 0, 0},
+                {0, 2, 0, 0, 2},
+                {0, 8, 0, 9, 0}
+            };
+		int array2[][] = {
+				{2, 3, 5, 0, 4},
+                {0, 4, 0, 3, 0},
+                {0, 0, 4, 0, 0},
+                {0, 2, 0, 0, 2},
+                {0, 8, 0, 9, 6}
+            };
+		int result[][] = {
+				{2, 35, 25, 36, 28},
+				{0, 22, 0, 12, 6 },
+				{0, 0, 0, 0, 0 },
+				{0, 24, 0, 24, 12 },
+				{0, 50, 0, 24, 18 }
+		};
+		int outputArray[][] = SparseMatrix.multiplyMatrices(array1, array2);
+		assertArrayEquals(result,outputArray);
+		
+	}
+	
+
+}
---
+++ SCF-Assignment7/src/SparseMatrix.java
@@ -0,0 +1,251 @@
+import java.util.*;
+
+/*
+ * @author Neha Patidar
+ */
+public class SparseMatrix {
+	final int[][] sparseMatrix;
+	final int noOfRows, noOfColumns, noOfNoneZeros;
+	
+	/*
+	 * Parameterized constructor
+	 * @param matrix
+	 */
+	
+	SparseMatrix(int nonZero,int [][]matrix){
+		
+		this.sparseMatrix = new int[nonZero][3];
+		this.noOfRows = matrix.length;
+		int tempVar = 0;
+		if(noOfRows != 0){
+			this.noOfColumns = matrix[0].length;
+			
+			for(int rowIndex = 0; rowIndex < noOfRows; rowIndex++){
+				for(int colIndex = 0; colIndex < matrix[0].length; colIndex++){
+					if(matrix[rowIndex][colIndex] != 0){
+						sparseMatrix[tempVar][0] = rowIndex;
+						sparseMatrix[tempVar][1] = colIndex;
+						sparseMatrix[tempVar][2] = matrix[rowIndex][colIndex];
+						tempVar++;
+					}
+				}
+			}	
+		}
+		else{
+			this.noOfColumns = 0;
+		}
+		this.noOfNoneZeros = tempVar;
+	}
+	/*
+	 *@return this.sparseMatrix
+	 */
+	public int[][] getSparse(){
+		return this.sparseMatrix;
+	}
+	/*
+	 * transpose of the matrix
+	 * @return transposeMatrix
+	 */
+	public int[][] getTranspose(){
+		int [][] transposeMatrix =null;
+		if(noOfRows == noOfColumns){
+			transposeMatrix = new int[noOfNoneZeros][3];
+			for(int index = 0; index < noOfNoneZeros; index++){
+				// swap row no with column no
+				transposeMatrix[index][0] = sparseMatrix[index][1];
+				transposeMatrix[index][1] = sparseMatrix[index][0];
+				transposeMatrix[index][2] = sparseMatrix[index][2];
+			}
+		}
+		return transposeMatrix;
+	}
+	public boolean isSymmetricalMatrix(){
+		int [][] original = sparseMatrix;
+		int transpose[][] = getTranspose();
+		if(original != null && transpose != null){
+			for(int i = 0; i < noOfNoneZeros;i++){
+				if(original[i][0] == transpose[i][1] && original[i][1] == transpose[i][0] &&original[i][2] == transpose[i][2]){
+					continue;
+				}else return false;
+			}
+		}
+		return true;
+	}
+	
+	/*
+	 * @param matrix1, matrix2
+	 * @return resultMatrix
+	 */
+	public static int[][] addMatrices(int[][] matrix1,int[][]matrix2){
+		int resultMatrix [][] = null;
+		
+		if(matrix1 != null && matrix2 != null){
+			if(matrix1.length == matrix2.length && matrix1[0].length == matrix2[0].length){
+				resultMatrix = new int[matrix1.length][matrix1[0].length];
+				for(int rowIndex = 0; rowIndex < matrix1.length; rowIndex++){
+					for(int colIndex = 0; colIndex < matrix1[rowIndex].length; colIndex++){
+						resultMatrix[rowIndex][colIndex] = matrix1[rowIndex][colIndex] + matrix2[rowIndex][colIndex];
+					}
+				}
+			}
+		}
+		return resultMatrix;
+	}
+	
+	/*
+	 * @param matrix1, matrix2
+	 * @return resultMatrix
+	 */
+	public static int[][] multiplyMatrices(int[][]matrix1, int[][]matrix2){
+		int [][] resultMatrix = null;
+		if(matrix1 != null && matrix2 != null){
+			if(matrix1[0].length == matrix2.length){
+				resultMatrix = new int[matrix1.length][matrix2[0].length];
+				for(int rowIndex = 0; rowIndex < matrix1.length; rowIndex++){
+					for(int  rowColInd = 0; rowColInd < matrix2.length; rowColInd++){
+						resultMatrix[rowIndex][rowColInd] = 0;
+						for(int colIndex = 0;colIndex < matrix2[0].length; colIndex++){
+							resultMatrix[rowIndex][rowColInd] += matrix1[rowIndex][colIndex] * matrix2[colIndex][rowColInd];
+						}
+					}
+				}
+			}
+		}
+		return resultMatrix;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter 0 to stop performing any operation");
+		System.out.println("Enter 1 to perform transpose the sparse matrix");
+		System.out.println("Enter 2 to check if matrix is symmetric or not");
+		System.out.println("Enter 3 to add two matrix");
+		System.out.println("Enter 4 to multiply 2 matrix");
+		int operationCase = scanner.nextInt();
+		while(operationCase != 0){
+			if(operationCase == 1){
+				//getTranspose()
+				int rows, columns, array[][];
+				System.out.println("Enter Number of rows and columns");
+				rows = scanner.nextInt();
+				columns = scanner.nextInt();
+				array = new int[rows][columns];
+				int nonZero = 0;
+				for(int rowIndex = 0; rowIndex < rows; rowIndex++){
+					for(int columnIndex = 0;columnIndex < columns; columnIndex++){
+						array[rowIndex][columnIndex] = scanner.nextInt();
+						if(array[rowIndex][columnIndex] != 0){
+							nonZero++;
+						}
+					}
+				}
+				SparseMatrix object = new SparseMatrix(nonZero,array);
+				int [][]sparse = object.getSparse();
+				for(int i = 0 ; i < sparse.length;i++){
+					for(int j = 0; j < 3;j++){
+						System.out.print(sparse[i][j] + " ");
+					}
+					System.out.println();
+				}
+				System.out.println("Transpose");
+				int[][] transpose = object.getTranspose();	
+				for(int i = 0 ; i < transpose.length;i++){
+					for(int j = 0; j < 3;j++){
+						System.out.print(transpose[i][j] + " ");
+					}
+					System.out.println();
+				}
+			}
+			else if (operationCase == 2){
+				// isSymmetricalMatrix()
+				int rows, columns, array[][];
+				System.out.println("Enter Number of rows and columns");
+				rows = scanner.nextInt();
+				columns = scanner.nextInt();
+				array = new int[rows][columns];
+				int nonZero = 0;
+				for(int rowIndex = 0; rowIndex < rows; rowIndex++){
+					for(int columnIndex = 0;columnIndex < columns; columnIndex++){
+						array[rowIndex][columnIndex] = scanner.nextInt();
+						if(array[rowIndex][columnIndex] != 0){
+							nonZero++;
+						}
+					}
+				}
+				SparseMatrix object = new SparseMatrix(nonZero,array);
+				System.out.println("Given matrix a symmetric or not : " + object.isSymmetricalMatrix());
+			}else if(operationCase == 3){
+				
+				int rows1, columns1, array1[][], rows2, columns2, array2[][];
+				System.out.println("Enter Number of rows and columns for first matrix : ");
+				rows1 = scanner.nextInt();
+				columns1 = scanner.nextInt();
+				array1 = new int[rows1][columns1];
+				System.out.println("Enter the elements in first matrix: ");
+				for(int rowIndex = 0; rowIndex < rows1; rowIndex++){
+					for(int columnIndex = 0;columnIndex < columns1; columnIndex++){
+						array1[rowIndex][columnIndex] = scanner.nextInt();
+					}
+				}
+				System.out.println("Enter Number of rows and columns for second matrix");
+				rows2 = scanner.nextInt();
+				columns2 = scanner.nextInt();
+				array2 = new int[rows1][columns1];
+				System.out.println("Enter the elements in first matrix: ");
+				for(int rowIndex = 0; rowIndex < rows2; rowIndex++){
+					for(int columnIndex = 0;columnIndex < columns2; columnIndex++){
+						array2[rowIndex][columnIndex] = scanner.nextInt();
+					}
+				}
+				
+				int addedMatrix[][] = addMatrices(array1, array2);
+				for(int rowIndex = 0; rowIndex < addedMatrix.length; rowIndex++){
+					for(int columnIndex = 0;columnIndex < addedMatrix[0].length; columnIndex++){
+						System.out.print(addedMatrix[rowIndex][columnIndex] + " ");
+					}
+					System.out.println();
+				}
+				
+			}else if (operationCase == 4){
+				
+				int rows1, columns1, array1[][], rows2, columns2, array2[][];
+				System.out.println("Enter Number of rows and columns for first matrix : ");
+				rows1 = scanner.nextInt();
+				columns1 = scanner.nextInt();
+				array1 = new int[rows1][columns1];
+				System.out.println("Enter the elements in first matrix: ");
+				for(int rowIndex = 0; rowIndex < rows1; rowIndex++){
+					for(int columnIndex = 0;columnIndex < columns1; columnIndex++){
+						array1[rowIndex][columnIndex] = scanner.nextInt();
+					}
+				}
+				System.out.println("Enter Number of rows and columns for second matrix");
+				rows2 = scanner.nextInt();
+				columns2 = scanner.nextInt();
+				array2 = new int[rows1][columns1];
+				System.out.println("Enter the elements in first matrix: ");
+				for(int rowIndex = 0; rowIndex < rows2; rowIndex++){
+					for(int columnIndex = 0;columnIndex < columns2; columnIndex++){
+						array2[rowIndex][columnIndex] = scanner.nextInt();
+					}
+				}
+				
+				int multiplyedMatrix[][] = multiplyMatrices(array1,array2);
+				for(int rowIndex = 0; rowIndex < multiplyedMatrix.length; rowIndex++){
+					for(int columnIndex = 0;columnIndex < multiplyedMatrix[0].length; columnIndex++){
+						System.out.print(multiplyedMatrix[rowIndex][columnIndex] + " ");
+					}
+					System.out.println();
+				}
+			}else{
+				System.out.println("Enter valid operation case");
+			}
+			System.out.println("Enter operation case from 0 to 4");
+			operationCase = scanner.nextInt();
+		}
+		
+		
+	}
+
+}
