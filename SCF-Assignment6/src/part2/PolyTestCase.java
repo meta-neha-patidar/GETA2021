3bdc5110fb8be70d895533dea2e4a3dd62a3fb7a
meta-neha-patidar <79203295+meta-neha-patidar@users.noreply.github.com>
2021-03-31 17:45:44 +0530

    Update PolyTestCase.java

--- SCF-Assignment6/src/part2/PolyTestCase.java
+++ SCF-Assignment6/src/part2/PolyTestCase.java
@@ -8,22 +8,22 @@ public class PolyTestCase {
 
 	@Test
 	public void degreeTestPositive() {
-		int arr[] = {1,2,3,0,5};
+		int arr[][] = {{2,1},{3,5},{4,2},{2,8}};
 		Polynomial poly = new Polynomial(arr);
-		assertEquals(4,poly.degree());
+		assertEquals(8,poly.degree());
 	}
 	@Test
 	public void degreeTestNegative() {
-		int arr[] = {1,2,3,0,5};
+		int arr[][] = {{2,1},{3,5},{4,2},{2,8}};
 		Polynomial poly = new Polynomial(arr);
 		assertNotEquals(6,poly.degree());
 	}
 	
 	@Test
 	public void addPolyTest(){
-		int arr1[] = {1,2,3,4};
-		int arr2[] = {6,5,4};
-		int result[] = {1,8,8,8};
+		int arr1[][] = {{1,2},{2,3},{4,4}};
+		int arr2[][] = {{4,2},{5,4}};
+		int result[][] = {{9,4},{2,3},{5,2}};
 		Polynomial poly1 = new Polynomial(arr1);
 		Polynomial poly2 = new Polynomial(arr2);
 		assertArrayEquals(result,Polynomial.addPolynomials(poly1, poly2));
@@ -31,9 +31,9 @@ public class PolyTestCase {
 	
 	@Test
 	public void productPolyTest(){
-		int arr1[] = {1,2,3,4};
-		int arr2[] = {5,6,7};
-		int result[] = {5, 16, 27, 18, 0, 0};
+		int arr1[][] = {{1,2},{2,3},{4,4}};
+		int arr2[][] = {{4,2},{5,4}};
+		int result[][] = {{8,5},{16,6},{10,7},{20,8}};
 		Polynomial poly1 = new Polynomial(arr1);
 		Polynomial poly2 = new Polynomial(arr2);
 		assertArrayEquals(result,Polynomial.multiplyPolynomials(poly1, poly2));
